a64fd55c775dbc2482098138ce70a3cd23909601
SethBlovits <sethblovits@gmail.com>
2026-03-18 09:54:30 -0400

    removed files

--- misc/setup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo setting up virtual drive and calling environement variables
-subst w: C:\EngineV2
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-echo %INCLUDE%
